36b0f8b6506da6812ec3c1c8647e4b9f42121428
brain64bit <brain64bit@9edddad6-f75a-11dd-a8e2-f7cb3c35fcff>
2009-02-12 07:11:49 +0000

    baru neeeh

---
+++ trunk/SpatoBiz/src/com/pos/spatobiz/app/controller/pemasok/TombolPemasokController.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.pos.spatobiz.app.controller.pemasok;
+
+import echo.gokil.desktop.application.Application;
+import echo.gokil.desktop.util.DesktopManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * jdk version : jdk 1.6.0_06
+ * OS : Ubuntu 8.04 i386
+ * email : brain64bit@gmail.com
+ * @author agung prasetyo, Planet Earth
+ * @date 12 Feb 09
+ */
+public class TombolPemasokController implements ActionListener
+{
+
+    public void actionPerformed(ActionEvent e) {
+        Application application = DesktopManager.getApplication();
+        application.showChildPane("menuPemasok");
+    }
+
+}
